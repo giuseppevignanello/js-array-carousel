70833dad497f63a3ae033eef7a0ed33967db8e2b
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-14 15:21:44 +0100

    problem analysis

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+
+// tools
+
+// array
+// for
+// if/else
+// console.log
+// const/let
+// addEventlistener
+// querySelector
+// getElementbyId
+
+const images = [
+    "./assets/img/01.webp",
+    "./assets/img/02.webp",
+    "./assets/img/03.webp",
+    "./assets/img/04.webp",
+    "./assets/img/05.webp",
+]
+//with CSS hide all images except one 
+// create a for to slide the images 
+// add images to HTML with templete literal and insertAdjacentHTML
+
+
+
+
+
+
