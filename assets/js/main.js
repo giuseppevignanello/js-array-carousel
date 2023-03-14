5185efe17d19b6c9c48611f1196c231bd4c1c225
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-14 15:51:57 +0100

    add next/prev event listener

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,23 +22,65 @@ const images = [
 
 const imagesElement = document.querySelector(".images"); 
 
-let activeImage = 0
+let activeImg = 0
 //with CSS hide all images except one 
 // create a for to slide the images 
 // add images to HTML with templete literal and insertAdjacentHTML
 for (i=0; i<images.length; i++) {
     const imgSrc = images[i]; 
-    const imageEl = `<img class = "${i === activeImage ? 'active' : ''}" src="${imgSrc}" alt="">`
+    const imageEl = `<img class = "${i === activeImg ? 'active' : ''}" src="${imgSrc}" alt="">`
     console.log(imageEl);
     imagesElement.insertAdjacentHTML("beforeend", imageEl)
 }
 
 
-// create a const fot displayed img 
+// create a const for displayed img 
+
+const displayedImgEl = document.querySelectorAll("img"); 
 // add event listener to next/prev 
+const nextEl = document.querySelector(".next"); 
+
+nextEl.addEventListener("click",
+
+function(){
 // remove the class active from the current img
+
+let currentImg = displayedImgEl[activeImg]; 
+
+currentImg.classList.remove("active");
 // increase/decrease activeImg counter
+
+activeImg++; 
 // assign class active to next/prev img 
+
+
+let nextImg = displayedImgEl[activeImg]; 
+nextImg.classList.add("active")
+})
+
+
+// add event listener to next/prev 
+const prevEl = document.querySelector(".prev"); 
+
+prevEl.addEventListener("click",
+
+function(){
+// remove the class active from the current img
+
+let currentImg = displayedImgEl[activeImg]; 
+
+currentImg.classList.remove("active");
+// increase/decrease activeImg counter
+
+activeImg--; 
+// assign class active to next/prev img 
+
+
+let prevImg = displayedImgEl[activeImg]; 
+prevImg.classList.add("active")
+})
+
+
 
 
 
