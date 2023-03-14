b6fdf32934efb3ec1724e73a54a918eea423d186
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-14 15:44:43 +0100

    problem analysis

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,14 +25,22 @@ const imagesElement = document.querySelector(".images");
 let activeImage = 0
 //with CSS hide all images except one 
 // create a for to slide the images 
-
+// add images to HTML with templete literal and insertAdjacentHTML
 for (i=0; i<images.length; i++) {
     const imgSrc = images[i]; 
     const imageEl = `<img class = "${i === activeImage ? 'active' : ''}" src="${imgSrc}" alt="">`
     console.log(imageEl);
     imagesElement.insertAdjacentHTML("beforeend", imageEl)
 }
-// add images to HTML with templete literal and insertAdjacentHTML
+
+
+// create a const fot displayed img 
+// add event listener to next/prev 
+// remove the class active from the current img
+// increase/decrease activeImg counter
+// assign class active to next/prev img 
+
+
 
 
 
